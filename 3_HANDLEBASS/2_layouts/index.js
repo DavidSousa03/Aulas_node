72bb52208e45944c98cec377daa5976fee775c9d
David Sousa <Davidm.sousa244@gmail.com>
2023-10-09 22:19:09 -0300

    Layouts no handlebars

--- 3_HANDLEBASS/2_layouts/index.js
+++ 3_HANDLEBASS/2_layouts/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.engine('handlebars', exphbs.engine())
 app.set("view engine", "handlebars")
 app.get("/", (req,res) => {
-    res.render('home', )
+    res.render('home')
 })
 
 app.listen(3000, ()=> {
